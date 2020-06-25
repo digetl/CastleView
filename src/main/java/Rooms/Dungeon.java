2cc2b8e8306030dc6eaa5055061ea8ad79c7f704
digetl <dave@digetl.co.uk>
2020-06-25 13:56:51 +0100

    Added Dungeon

--- src/main/java/Rooms/Dungeon.java
+++ src/main/java/Rooms/Dungeon.java
@@ -1,4 +1,12 @@
 package Rooms;
 
-public class Dungeon {
+import Items.Items;
+
+import java.util.ArrayList;
+
+public class Dungeon extends Room{
+
+    public Dungeon(String name, ArrayList<Items> roomContents) {
+        super(name, roomContents);
+    }
 }
--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,4 +1,17 @@
 package Rooms;
 
+import Items.Items;
+
+import java.util.ArrayList;
+
 public abstract class Room {
+
+    private String name;
+    private ArrayList<Items> roomContents;
+
+    public Room(String name, ArrayList<Items> roomContents){
+        this.name = name;
+        this.roomContents = new ArrayList<Items>();
+    }
+
 }
