bae1bb086166cff2af3c22c15571eeec55696fba
digetl <dave@digetl.co.uk>
2020-06-25 14:41:40 +0100

    test - Dungeon canAddItemToRoom - passed

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,6 +1,7 @@
 package Rooms;
 
 import Items.Items;
+import Players.Player;
 
 import java.util.ArrayList;
 
@@ -8,10 +9,12 @@ public abstract class Room {
 
     private String name;
     private ArrayList<Items> roomContents;
+    private ArrayList<Player> roomPlayers;
 
     public Room(String name, ArrayList<Items> roomContents){
         this.name = name;
         this.roomContents = new ArrayList<Items>();
+        this.roomPlayers = new ArrayList<Player>();
     }
 
 
@@ -19,5 +22,19 @@ public abstract class Room {
         return name;
     }
 
+    public int roomContentCount(){
+        return roomContents.size();
+    }
 
+    public int roomPlayersCount(){
+        return roomPlayers.size();
+    }
+
+    public void addPlayerToRoom(Player player){
+        this.roomPlayers.add(player);
+    }
+
+    public void addItemToRoom(Items item){
+        this.roomContents.add(item);
+    }
 }
--- src/test/java/DungeonTest.java
+++ src/test/java/DungeonTest.java
@@ -1,7 +1,9 @@
 import Items.Items;
 import Items.Weapons.BigClaw;
+import Items.Weapons.Sword;
 import Items.Weapons.WeeClaw;
 import Players.Enemies.Monster;
+import Players.Heroes.Knight;
 import Rooms.Dungeon;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +13,11 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class DungeonTest {
+
+    Sword sword1;
+    Sword sword2;
+
+    Knight knight1;
 
     Dungeon dungeon;
 
@@ -25,7 +32,14 @@ public class DungeonTest {
         weeClaw = new WeeClaw("Wee Claw", 10, 0);
 
         monster1 = new Monster("Hairy McClairy", 50, bigClaw, weeClaw);
+
+        sword1 = new Sword("Stabby McStabby", 20, 0);
+        sword2 = new Sword("Butterknife", 5, 0);
+
+        knight1 = new Knight("Sir Bob",100, sword1, sword2);
+
         dungeon = new Dungeon("Combat Hall", roomContents);
+
     }
 
     @Test
@@ -33,6 +47,27 @@ public class DungeonTest {
         assertEquals("Combat Hall", dungeon.getName());
     }
 
+    @Test
+    public void checksRoomStartsEmpty__OfContents() {
+        assertEquals(0, dungeon.roomContentCount());
+    }
+
+    @Test
+    public void checksRoomStartsEmpty__OfPlayers() {
+        assertEquals(0, dungeon.roomPlayersCount());
+    }
+
+    @Test
+    public void canAddPlayerToRoom() {
+        dungeon.addPlayerToRoom(knight1);
+        assertEquals(1, dungeon.roomPlayersCount());
+    }
+
+    @Test
+    public void canAddItemToRoom() {
+        dungeon.addItemToRoom(sword1);
+        assertEquals(1, dungeon.roomContentCount());
+    }
 
 
 }
